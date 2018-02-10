f978019aabcffb937eacef1803f89dce19568b14
Robyn Choi <robyn3choi@gmail.com>
2018-02-10 12:16:57 -0800

    pixelmanager, also started ui

---
+++ Assets/Scripts/PixelManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelManager : MonoBehaviour {
+
+    List<Pixel> pixels = new List<Pixel>();
+    public float timeBetweenPixelFades = 1; // will gradually decrease by GameManager
+    float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+        // get all the Pixel components in my children and add to my pixel list
+        foreach (Transform t in transform) {
+            Pixel pixel = t.GetComponent<Pixel>();
+            pixels.Add(pixel);
+        }
+
+        StartCoroutine(FadeRandomPixels());
+	}
+
+    IEnumerator FadeRandomPixels() {
+        while (true) {
+            if (timer >= timeBetweenPixelFades) {
+                Pixel randomPixel = GetRandomPixel();
+                // TODO: replace with actual pixel's method
+                randomPixel.Fade();
+                timer = timeBetweenPixelFades;
+            }
+            else {
+                timer += Time.deltaTime;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    Pixel GetRandomPixel() {
+        int randomIndex = Random.Range(0, pixels.Count);
+        return pixels[randomIndex];
+    }
+}
